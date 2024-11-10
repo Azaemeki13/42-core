c1f58e9b94f3720490d0bd8fdaabe7a88aca16c5
ituriel <ituriel@CharlesPC.>
2024-11-10 13:44:03 +0100

    getnextline

---
+++ circle_1/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ituriel <ituriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 13:37:17 by ituriel           #+#    #+#             */
+/*   Updated: 2024/11/10 13:41:39 by ituriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
