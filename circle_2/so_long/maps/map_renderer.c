58d4e290b06b82b3ee3fb47cf9f1c64d5bda9d91
Charles Auffret <cauffret@c1r7s4.42luxembourg.lu>
2025-02-28 13:53:31 +0100

    map parser + gnl changed.

--- circle_2/so_long/maps/map_renderer.c
+++ circle_2/so_long/maps/map_renderer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_renderer.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ituriel <ituriel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cauffret <cauffret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 20:01:21 by ituriel           #+#    #+#             */
-/*   Updated: 2025/02/25 17:05:10 by ituriel          ###   ########.fr       */
+/*   Updated: 2025/02/28 13:38:08 by cauffret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,3 @@
 
 // import map into a grid 
 
-char **ber_to_grid(char *name)
-{
-    int fd;
-    int i;
-    int height;
-    char *length;
-
-    length = NULL;
-    i = 1;
-    height = 1;
-    fd = open(name, O_RDONLY);
-    length = get_next_line(fd);
-    i = ft_strlen(length);
-    // i here represent my width
-    while (get_next_line(fd))
-        height++;
-    i = 1920 / i;
-    height = 1080 / height;
-
-}
